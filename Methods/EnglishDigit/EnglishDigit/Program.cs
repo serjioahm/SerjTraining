e84d3700ad084d1d659d719e6eb52f0a963642e5
serjioahm <dirakont@abv.bg>
2016-08-25 17:28:58 +0300

    Added 2 tasks of Methods.

---
+++ Methods/EnglishDigit/EnglishDigit/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnglishDigit
+{
+    class Program
+    {
+        //Write a method that returns the last digit of given integer as an English word.
+        //Write a program that reads a number and prints the result of the method.
+        //On the first line you will receive a number
+        //Print the last digit of the number as an English word
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(LastEnglishDigit(number));
+        }
+
+        private static string LastEnglishDigit(int number)
+        {
+            var word = string.Empty;
+            number = number % 10;
+            switch (number)
+            {
+                case 0:
+                    word = "zero";
+                    break;
+                case 1:
+                    word = "one";
+                    break;
+                case 2:
+                    word = "two";
+                    break;
+                case 3:
+                    word = "three";
+                    break;
+                case 4:
+                    word = "four";
+                    break;
+                case 5:
+                    word = "five";
+                    break;
+                case 6:
+                    word = "six";
+                    break;
+                case 7:
+                    word = "seven";
+                    break;
+                case 8:
+                    word = "eight";
+                    break;
+                case 9:
+                    word = "nine";
+                    break;
+            }
+
+            return word;
+        }
+
+
+    }
+}
---
+++ Methods/LargerThanNeighbours/LargerThanNeighbours/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LargerThanNeighbours
+{
+    class Program
+    {
+        //Write a method that checks if the element at given position in given array of integers is larger than its two neighbours (when such exist).
+        //Write program that reads an array of numbers and prints how many of them are larger than their neighbours.
+        //On the first line you will receive the number N - the size of the array
+        //On the second line you will receive N numbers separated by spaces - the array
+        //Print how many numbers in the array are larger than their neighbours
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            var length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter array elements ...");
+            int[] array = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var counter = CountBiggerThanNeighbours(array);
+
+            Console.WriteLine("The numbers that are larger than their neighbours are :"+counter);
+        }
+
+        private static int CountBiggerThanNeighbours(int[] numbers)
+        {
+            var counter = 0;
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
